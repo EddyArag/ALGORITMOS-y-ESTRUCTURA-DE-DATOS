d829fe9cca8738d36277eac061370512b5436e58
JoacoYa22 <jyatomarroquin@gmail.com>
2025-03-17 17:42:15 -0500

    Update ContainerRect.java

--- Actividad/ContainerRect.java
+++ Actividad/ContainerRect.java
@@ -1,3 +1,4 @@
+package Actividad;
 // Clase contenedora de rectángulos
 class ContainerRect {
     private Rectangulo[] rectangulos;
@@ -5,8 +6,7 @@ class ContainerRect {
     private double[] areas;
     private int numRec;
 
-    public ContainerRect(int capacidad) {
-        this.n = capacidad;
+    public ContainerRect() {
         this.rectangulos = new Rectangulo[2];
         this.distancias = new double[2];
         this.areas = new double[2];
@@ -14,22 +14,25 @@ class ContainerRect {
     }
 
     public void addRectangulo(Rectangulo r) {
-        if (numRec < n) {
+        if (numRec < 2) {
             rectangulos[numRec] = r;
             distancias[numRec] = r.getEsquina1().distancia(r.getEsquina2());
             areas[numRec] = r.calcularArea();
             numRec++;
         } else {
-            System.out.println("Solo se pueden almacenar dos rectángulo");
+            System.out.println("Solo se pueden almacenar dos rectangulos");
         }
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("Rectángulo\tCoordenadas\tDistancia\tÁrea\n");
+        sb.append("Rectangulo\tCoordenadas\t\t\tDistancia\tArea\n");
         for (int i = 0; i < numRec; i++) {
-            sb.append((i + 1) + "\t" + rectangulos[i] + "\t" + String.format("%.3f", distancias[i]) + "\t" + String.format("%.2f", areas[i]) + "\n");
+            Coordenada e1 = rectangulos[i].getEsquina1();
+            Coordenada e2 = rectangulos[i].getEsquina2();
+            sb.append((i + 1) + "\t\t(" + e1.getX() + ", " + e1.getY() + ") - (" + e2.getX() + ", " + e2.getY() + ")"
+                    + "\t" + String.format("%.3f", distancias[i]) + "\t" + "\t" + String.format("%.2f", areas[i]) + "\n");
         }
         return sb.toString();
     }
