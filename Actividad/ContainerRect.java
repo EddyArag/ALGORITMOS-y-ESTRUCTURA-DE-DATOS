7e0b966acab9cdfd2b354dad0207a76f42a071a7
EddyArag <164369960+EddyArag@users.noreply.github.com>
2025-03-17 18:02:01 -0500

    Update ContainerRect.java

--- Actividad/ContainerRect.java
+++ Actividad/ContainerRect.java
@@ -1,12 +1,13 @@
-package Actividad;
 // Clase contenedora de rectángulos
 class ContainerRect {
     private Rectangulo[] rectangulos;
     private double[] distancias;
     private double[] areas;
     private int numRec;
+    private int n;
 
-    public ContainerRect() {
+    public ContainerRect(int capacidad) {
+        this.n = capacidad;
         this.rectangulos = new Rectangulo[2];
         this.distancias = new double[2];
         this.areas = new double[2];
@@ -14,21 +15,23 @@ class ContainerRect {
     }
 
     public void addRectangulo(Rectangulo r) {
-        rectangulos[numRec] = r;
-        distancias[numRec] = r.getEsquina1().distancia(r.getEsquina2());
-        areas[numRec] = r.calcularArea();
-        numRec++;
+        if (numRec < n) {
+            rectangulos[numRec] = r;
+            distancias[numRec] = r.getEsquina1().distancia(r.getEsquina2());
+            areas[numRec] = r.calcularArea();
+            numRec++;
+        } else {
+            System.out.println("Solo se pueden almacenar dos rectángulo");
+        }
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("Rectangulo\tCoordenadas\t\t\tDistancia\tArea\n");
+        sb.append("Rectángulo\tCoordenadas\tDistancia\tÁrea\n");
         for (int i = 0; i < numRec; i++) {
-            Coordenada e1 = rectangulos[i].getEsquina1();
-            Coordenada e2 = rectangulos[i].getEsquina2();
-            sb.append((i + 1) + "\t\t(" + e1.getX() + ", " + e1.getY() + ") - (" + e2.getX() + ", " + e2.getY() + ")"
-                    + "\t" + String.format("%.3f", distancias[i]) + "\t" + "\t" + String.format("%.2f", areas[i]) + "\n");
+            sb.append((i + 1) + "\t" + rectangulos[i] + "\t" + String.format("%.3f", distancias[i]) + "\t"
+                    + String.format("%.2f", areas[i]) + "\n");
         }
         return sb.toString();
     }
