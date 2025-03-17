8f6f043821523b1a3c929ae36b25c3409daaaf57
EddyArag <164369960+EddyArag@users.noreply.github.com>
2025-03-17 18:02:48 -0500

    Update Verificador.java

--- Actividad/Verificador.java
+++ Actividad/Verificador.java
@@ -1,5 +1,3 @@
-ackage Actividad;
-
 // Clase que verifica la relación entre dos rectángulos
 class Verificador {
     public static boolean SobrePuestos(Rectangulo a, Rectangulo b) {
@@ -11,18 +9,26 @@ class Verificador {
         }
         return false;
     }
+
     public static boolean esJuntos(Rectangulo a, Rectangulo b) {
 
         // Caso 2: Los rectángulos están juntos si comparten un lado en común sin
         // superponerse
-        if ((SobrePuestos(a,b)) || (a.getEsquina2().getX() == b.getEsquina1().getX() || a.getEsquina1().getX() == b.getEsquina2().getX()) || (a.getEsquina2().getY() == b.getEsquina1().getY() || a.getEsquina1().getY() == b.getEsquina2().getY())   ) {
+        if (((a.getEsquina1().getX() <= b.getEsquina2().getX() && a.getEsquina2().getX() >= b.getEsquina1().getX()) &&
+                (a.getEsquina1().getY() <= b.getEsquina2().getY() && a.getEsquina2().getY() >= b.getEsquina1().getY()))
+
+                && ((a.getEsquina2().getX() == b.getEsquina1().getX()
+                        || a.getEsquina1().getX() == b.getEsquina2().getX())
+                        || (a.getEsquina2().getY() == b.getEsquina1().getY()
+                                || a.getEsquina1().getY() == b.getEsquina2().getY()))) {
             return true;
         }
         return false;
     }
+
     public static boolean Disjuntos(Rectangulo a, Rectangulo b) {
         // Caso 3: Los rectángulos son disjuntos si no tienen contacto ni se sobreponen
-        if(esJuntos(a, b) || SobrePuestos(a, b)) {
+        if (esJuntos(a, b) || SobrePuestos(a, b)) {
             return false;
         }
         return true;
