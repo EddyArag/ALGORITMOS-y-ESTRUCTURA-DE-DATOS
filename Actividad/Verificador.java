6a5a5a37b11f11ae36bb0ffab6a62915f63f286e
EddyArag <164369960+EddyArag@users.noreply.github.com>
2025-03-17 17:27:47 -0500

    Update Verificador.java

--- Actividad/Verificador.java
+++ Actividad/Verificador.java
@@ -3,14 +3,13 @@ package Actividad;
 // Clase que verifica la relación entre dos rectángulos
 class Verificador {
     public static String verificarRelacion(Rectangulo a, Rectangulo b) {
-        // Caso 1: Los rectángulos se sobreponen si comparten un área en común
+        
         if ((a.getEsquina1().getX() < b.getEsquina2().getX() && a.getEsquina2().getX() > b.getEsquina1().getX()) &&
                 (a.getEsquina1().getY() < b.getEsquina2().getY() && a.getEsquina2().getY() > b.getEsquina1().getY())) {
             return "Los rectángulos A y B se sobreponen.";
         }
 
-        // Caso 2: Los rectángulos están juntos si comparten un lado en común sin
-        // superponerse
+
         else if ((a.getEsquina2().getX() == b.getEsquina1().getX() || a.getEsquina1().getX() == b.getEsquina2().getX())
                 ||
                 (a.getEsquina2().getY() == b.getEsquina1().getY()
@@ -18,9 +17,28 @@ class Verificador {
             return "Los rectángulos A y B están juntos.";
         }
 
-        // Caso 3: Los rectángulos son disjuntos si no tienen contacto ni se sobreponen
+
         else {
             return "Los rectángulos A y B son disjuntos.";
         }
+    }
+}
+package Actividad;
+
+// Clase que verifica la relación entre dos rectángulos
+class Verificador {
+    // Caso 1: Los rectángulos se sobreponen si comparten un área en común
+    public static boolean esSobrePos(Rectangulo a, Rectangulo b) {
+        return (a.getEsquina1().getX() < b.getEsquina2().getX() && a.getEsquina2().getX() > b.getEsquina1().getX()) &&
+               (a.getEsquina1().getY() < b.getEsquina2().getY() && a.getEsquina2().getY() > b.getEsquina1().getY());
+    }
+    // Caso 2: Los rectángulos están juntos si comparten un lado en común sin superponerse
+    public static boolean esJunto(Rectangulo a, Rectangulo b) {
+        return (a.getEsquina2().getX() == b.getEsquina1().getX() || a.getEsquina1().getX() == b.getEsquina2().getX()) ||
+               (a.getEsquina2().getY() == b.getEsquina1().getY() || a.getEsquina1().getY() == b.getEsquina2().getY());
+    }
+    // Caso 3: Los rectángulos son disjuntos si no tienen contacto ni se sobreponen
+    public static boolean esDisjunto(Rectangulo a, Rectangulo b) {
+        return !esSobrePos(a, b) && !esJunto(a, b);
     }
 }
